1882861c17b501a256f87afc8bc0d04b5209b098
Ola-22 <olanazly@gmail.com>
2021-06-29 19:19:14 +0300

    Add Firebase configuration object

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBjh-6P1kKYGIc9M0M7v-_2n7cKVPvG-s0",
+  authDomain: "clone-aeac4.firebaseapp.com",
+  projectId: "clone-aeac4",
+  storageBucket: "clone-aeac4.appspot.com",
+  messagingSenderId: "939438313715",
+  appId: "1:939438313715:web:3260150fb160c19678a786",
+  measurementId: "G-L5MZRLQJ44",
+};
