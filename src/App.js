1ef4122d5d5860d2bc4aa4d58909865fbabc093a
Ola-22 <olanazly@gmail.com>
2021-07-03 17:06:44 +0300

    Add Router in app file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import "./App.css";
 import Header from "./Components/Header";
+import Sidebar from "./Components/Sidebar";
+import RecommendedVideos from "./Components/RecommendedVideos";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import SearchPage from "./Components/SearchPage";
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/search/:searchTerm">
+            <div className="app__page">
+              <Sidebar />
+              <SearchPage />
+            </div>
+          </Route>
+          <Route path="/">
+            <div className="app__page">
+              <Sidebar />
+              <RecommendedVideos />
+            </div>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
